ef4577b7cd5304872f542c6ac77540c692c71a02
daraphista <daraphistadaone@gmail.com>
2022-04-13 17:38:56 +0800

    feat(App): add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Search from "./routes/Search";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  padding-bottom: 2rem;
+`
 
 export default App;
