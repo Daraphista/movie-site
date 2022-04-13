7ddc58464c80ed7cb5fad0fd396968028a4cbfb1
daraphista <daraphistadaone@gmail.com>
2022-04-13 17:48:51 +0800

    feat(App): create standalone MovieResults component to display movies

---
+++ src/components/MovieResults.js
@@ -0,0 +1,74 @@
+import { FaPlay } from "react-icons/fa";
+import styled from "styled-components";
+
+const MovieResults = (props) => {
+  const { movies } = props;
+
+  return (
+    <MovieResultsContainer>
+      {movies.map(movie => {
+        return (
+            <MovieCard>
+              <MoviePoster poster={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
+              <TitleCard>{movie.title} <FaPlay /></TitleCard>
+            </MovieCard>
+        );
+      })}
+    </MovieResultsContainer>
+  );
+};
+
+const MovieResultsContainer = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 350px);
+  gap: 1rem;
+  justify-content: center;
+`
+
+const MovieCard = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  
+  height: 450px;  
+  cursor: pointer;
+`
+
+const MoviePoster = styled.div`
+  flex: 1;
+  background-image: ${(props) => `url(${props.poster})`};
+  background-size: cover;
+  background-position: center;
+  
+  ${MovieCard}:hover & {
+    filter: blur(5px)
+  }
+`
+
+const TitleCard = styled.div`
+  position: absolute;
+  display: none;
+
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%);
+
+  font-size: 1.5rem;
+  color: white;
+  text-align: center;
+
+  width: 100%;
+
+  justify-content: center;
+  align-items: center;
+  gap: .5rem;
+  
+  ${MovieCard}:hover & {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export default MovieResults;
