279ba7101226f518147a2ddc424788ada9281bcf
daraphista <daraphistadaone@gmail.com>
2022-04-19 14:08:00 +0800

    feat(App): add ability to watch tv shows

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import MovieCards from "../components/MovieCards";
+import MovieAndTVCards from "../components/MovieAndTVCards";
 
 const Home = (props) => {
   const { getMovieId } = props;
@@ -11,7 +11,6 @@ const Home = (props) => {
       const result = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=394a500dc1b768ebe40e5c02328b32bf");
       const data = await result.json();
       setRecommendedMovies(data.results);
-      console.log(data);
     } catch (error) {
       console.log("Error:", error);
     }
@@ -24,7 +23,6 @@ const Home = (props) => {
       const result = await fetch("https://api.themoviedb.org/3/tv/popular?api_key=394a500dc1b768ebe40e5c02328b32bf&language=en-US&page=1");
       const data = await result.json();
       setRecommendedShows(data.results);
-      console.log(data);
     } catch (error) {
       console.log("Error:", error);
     }
@@ -37,7 +35,7 @@ const Home = (props) => {
 
   return (
     <div>
-      <MovieCards movies={recommendedMovies} shows={recommendedShows} getMovieId={getMovieId} />
+      <MovieAndTVCards movies={recommendedMovies} shows={recommendedShows} getMovieId={getMovieId} />
     </div>
   )
 }
